7ca885f3886ef4dc4341b76dd824ddc170d6b84a
BrennanIannandrea <33362089+BrennanIannandrea@users.noreply.github.com>
2019-02-04 21:27:49 -0500

    changed program to do right thing

--- src/fiboseriesite/FiboSeriesIte.java
+++ src/fiboseriesite/FiboSeriesIte.java
@@ -7,16 +7,24 @@ public class FiboSeriesIte {
         System.out.println("Insert number of values:");
         long b = a.nextLong();
         long tim = System.currentTimeMillis();
+        if(b>0){
+            System.out.println("0");
+            b--;
+            if (b>0){
+                System.out.println("1");
+                b--;
+            }
+        }
         long numberOne = 1;
         long  numberTwo = 1;
-        System.out.println(numberOne);
-        System.out.println(numberTwo);
-        while (numberOne + numberTwo <= b){
+        while (b>0){
         numberOne= numberOne+numberTwo;
         System.out.println(numberOne);
-        if(numberOne+numberTwo<= b){
+        b--;
+        if(b>0){
         numberTwo= numberOne+numberTwo;
         System.out.println(numberTwo);
+        b--;
         }
         }
         long sTim = System.currentTimeMillis();
